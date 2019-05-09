01d1930e35809322b58e0b427f582189afbea736
pxdnowak <36161994+pxdnowak@users.noreply.github.com>
2019-05-09 13:01:08 +0200

    Adress, Adresscustomer

---
+++ TrafficWatchRest/TrafficWatchRest/Model/AdressCustomer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrafficWatchRest.Model
+{
+    public class AdressCustomer
+    {
+    }
+}
