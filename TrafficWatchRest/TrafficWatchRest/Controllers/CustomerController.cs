0094791f7fe55a0b44730a101c0b1df2e2983d83
pxdnowak <36161994+pxdnowak@users.noreply.github.com>
2019-05-09 13:33:43 +0200

    costumercontroller

--- TrafficWatchRest/TrafficWatchRest/Controllers/CustomerController.cs
+++ TrafficWatchRest/TrafficWatchRest/Controllers/CustomerController.cs
@@ -4,35 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using GoogleApi;
 
 namespace TrafficWatchRest.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ForsinkelsesController : ControllerBase
+    public class CustomerController : ControllerBase
     {
-        // GET: api/Forsinkelses
+        
+        // GET: api/Customer
         [HttpGet]
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Forsinkelses/5
+        // GET: api/Customer/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
             return "value";
         }
 
-        // POST: api/Forsinkelses
+        // POST: api/Customer
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT: api/Forsinkelses/5
+        // PUT: api/Customer/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
