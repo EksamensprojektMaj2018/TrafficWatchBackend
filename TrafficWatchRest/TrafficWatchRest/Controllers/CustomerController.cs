0b1a983a1e0d7adc77ba5a4d43e112bb462c1748
Jacob Larsen <captainpowerturtle@gmail.com>
2019-05-10 12:00:06 +0200

    Ting

--- TrafficWatchRest/TrafficWatchRest/Controllers/CustomerController.cs
+++ TrafficWatchRest/TrafficWatchRest/Controllers/CustomerController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using TrafficWatchRest.Model;
 
 namespace TrafficWatchRest.Controllers
 {
@@ -11,19 +12,25 @@ namespace TrafficWatchRest.Controllers
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        
+        private static List<Customer> customerList = new List<Customer>
+        {
+            new Customer(){Id = 1, Firstname = "Marcel", Lastname = "Kristensen", Email = "mar@mar.dk", AdresseID = 33, AlarmID = 8000, RouteID = 7, Admin = false},
+            new Customer(),
+            new Customer()
+        };
+
         // GET: api/Customer
         [HttpGet]
-        public IEnumerable<string> Get()
+        public List<Customer> Get()
         {
-            return new string[] { "value1", "value2" };
+            return customerList;
         }
 
         // GET: api/Customer/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public Customer Get(int id)
         {
-            return "value";
+            return customerList.FirstOrDefault(x => x.Id == id);
         }
 
         // POST: api/Customer
--- TrafficWatchRest/TrafficWatchRest/Model/Customer.cs
+++ TrafficWatchRest/TrafficWatchRest/Model/Customer.cs
@@ -4,13 +4,12 @@
     {
         public int Id { get; set; }
         public string Email { get; set; }
-        public string Navn { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
         public int AdresseID { get; set; }
         public int AlarmID { get; set; }
         public int RouteID { get; set; }
         public bool Admin { get; set; }
 
-        public Customer() { }
-     
     }
 }
