f4f543cf1bb2007222ee93394c64c7628c3e3fa2
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-05-22 13:59:17 +0200

    Fjernet CustomerAlarm variabler

--- TrafficWatchRest/TrafficWatchRest/Model/Alarm.cs
+++ TrafficWatchRest/TrafficWatchRest/Model/Alarm.cs
@@ -5,15 +5,8 @@ namespace TrafficWatchRest.Model
 {
     public partial class Alarm
     {
-        public Alarm()
-        {
-            CustomerAlarm = new HashSet<CustomerAlarm>();
-        }
-
         public int Id { get; set; }
         public DateTime WakeUp { get; set; }
         public int? Delay { get; set; }
-
-        public virtual ICollection<CustomerAlarm> CustomerAlarm { get; set; }
     }
 }
