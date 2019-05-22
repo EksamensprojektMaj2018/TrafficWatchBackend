16f3e9d6245827574d42d163a75b3c47950cc0ad
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-05-22 23:49:41 +0200

    Tilføjet parametre til Customer customer = new Customer()
    Indkommenteret GetCustomerById
    
    Fixet InsertCustomer så den virker
    
    Fixet UpdateCustomer så den virker

--- TrafficWatchRest/TrafficWatchRest/Controllers/CustomerController.cs
+++ TrafficWatchRest/TrafficWatchRest/Controllers/CustomerController.cs
@@ -27,7 +27,7 @@ namespace TrafficWatchRest.Controllers
             int alarmid = reader.IsDBNull(6) ? 0 : reader.GetInt32(6);
             int routeid = reader.IsDBNull(7) ? 0 : reader.GetInt32(7);
             bool admin = reader.GetBoolean(8);
-            Customer customer = new Customer
+            Customer customer = new Customer(id, googleid, email, firstname, lastname, admin)
             {
                 Id = id,
                 GoogleId = googleid,
@@ -67,25 +67,25 @@ namespace TrafficWatchRest.Controllers
         }
 
         // GET: api/Customer/5
-        //[Route("{id}")]
-        //public Customer GetCustomerById(int id)
-        //{
-        //    const string selectString = "select * from customer where id=@id";
-        //    using (SqlConnection databaseConnection = new SqlConnection(ConnectionString))
-        //    {
-        //        databaseConnection.Open();
-        //        using (SqlCommand selectCommand = new SqlCommand(selectString, databaseConnection))
-        //        {
-        //            selectCommand.Parameters.AddWithValue("@id", id);
-        //            using (SqlDataReader reader = selectCommand.ExecuteReader())
-        //            {
-        //                if (!reader.HasRows) { return null; }
-        //                reader.Read(); // advance cursor to first row
-        //                return ReadCustomer(reader);
-        //            }
-        //        }
-        //    }
-        //}
+        [Route("{id}")]
+        public Customer GetCustomerById(int id)
+        {
+            const string selectString = "select * from customer where id=@id";
+            using (SqlConnection databaseConnection = new SqlConnection(ConnectionString))
+            {
+                databaseConnection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectString, databaseConnection))
+                {
+                    selectCommand.Parameters.AddWithValue("@id", id);
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        if (!reader.HasRows) { return null; }
+                        reader.Read(); // advance cursor to first row
+                        return ReadCustomer(reader);
+                    }
+                }
+            }
+        }
         // GET: api/Customer/5
         [Route("{googleId}")]
         public Customer GetCustomerByGoogleId(string googleId)
@@ -109,22 +109,19 @@ namespace TrafficWatchRest.Controllers
 
         // POST: api/Customer
         [HttpPost]
-        public int AddCustomer([FromBody] Customer value)
+        public int InsertCustomer([FromBody] string googleId, string email, string firstName, string lastName, bool admin)
         {
-            const string insertString = "insert into Customer (google_id, email, first_name, last_name, address_id, alarm_id, route_id, administrator) values (@googleid, @email, @firstname, @lastname, @addressid, @alarmid, @routeid, @admin)";
+            const string insertString = "insert into Customer (google_id, email, first_name, last_name, administrator) values (@googleid, @email, @firstname, @lastname, @admin)";
             using (SqlConnection databaseConnection = new SqlConnection(ConnectionString))
             {
                 databaseConnection.Open();
                 using (SqlCommand insertCommand = new SqlCommand(insertString, databaseConnection))
                 {
-                    insertCommand.Parameters.AddWithValue("@googleid", value.GoogleId);
-                    insertCommand.Parameters.AddWithValue("@email", value.Email);
-                    insertCommand.Parameters.AddWithValue("@firstname", value.FirstName);
-                    insertCommand.Parameters.AddWithValue("@lastname", value.LastName);
-                    insertCommand.Parameters.AddWithValue("@addressid", value.AddressId);
-                    insertCommand.Parameters.AddWithValue("@alarmid", value.AlarmId);
-                    insertCommand.Parameters.AddWithValue("@routeid", value.RouteId);
-                    insertCommand.Parameters.AddWithValue("@admin", value.Administrator);
+                    insertCommand.Parameters.AddWithValue("@googleid", googleId);
+                    insertCommand.Parameters.AddWithValue("@email", email);
+                    insertCommand.Parameters.AddWithValue("@firstname", firstName);
+                    insertCommand.Parameters.AddWithValue("@lastname", lastName);
+                    insertCommand.Parameters.AddWithValue("@admin", admin);
                     int rowsAffected = insertCommand.ExecuteNonQuery();
                     return rowsAffected;
                 }
@@ -133,22 +130,22 @@ namespace TrafficWatchRest.Controllers
 
         // PUT: api/Customer/5
         [HttpPut("{id}")]
-        public int UpdateCustomer(int id, [FromBody] Customer value)
+        public int UpdateCustomer([FromBody]int id, string email, string firstName, string lastName, int addressId, int alarmId, int routeId )
         {
             const string updateString =
-                "update customer set email=@email, firstname=@firstname, lastname=@lastname, addressid=@addressid, alarmid=@alarmid routeid=@routeid, admin=@admin where id=@id;";
+                "update customer set email=@email, first_name=@firstname, last_name=@lastname, address_id=@addressid, alarm_id=@alarmid, route_id=@routeid where id=@id;";
             using (SqlConnection databaseConnection = new SqlConnection(ConnectionString))
             {
                 databaseConnection.Open();
                 using (SqlCommand updateCommand = new SqlCommand(updateString, databaseConnection))
                 {
-                    updateCommand.Parameters.AddWithValue("@email", value.Email);
-                    updateCommand.Parameters.AddWithValue("@firstname", value.FirstName);
-                    updateCommand.Parameters.AddWithValue("@lastname", value.LastName);
-                    updateCommand.Parameters.AddWithValue("@addressid", value.AddressId);
-                    updateCommand.Parameters.AddWithValue("@alarmid", value.AlarmId);
-                    updateCommand.Parameters.AddWithValue("@routeid", value.RouteId);
-                    updateCommand.Parameters.AddWithValue("@admin", value.Administrator);
+                    updateCommand.Parameters.AddWithValue("@id", id);
+                    updateCommand.Parameters.AddWithValue("@email", email);
+                    updateCommand.Parameters.AddWithValue("@firstname", firstName);
+                    updateCommand.Parameters.AddWithValue("@lastname", lastName);
+                    updateCommand.Parameters.AddWithValue("@addressid", addressId);
+                    updateCommand.Parameters.AddWithValue("@alarmid", alarmId);
+                    updateCommand.Parameters.AddWithValue("@routeid", routeId);
                     int rowsAffected = updateCommand.ExecuteNonQuery();
                     return rowsAffected;
                 }
