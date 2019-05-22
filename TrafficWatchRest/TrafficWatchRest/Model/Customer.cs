eec5910c82651ad5c61a82831505becbe958de82
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-05-22 23:49:59 +0200

    Tilføjet instance fields og en constructor

--- TrafficWatchRest/TrafficWatchRest/Model/Customer.cs
+++ TrafficWatchRest/TrafficWatchRest/Model/Customer.cs
@@ -5,6 +5,22 @@ namespace TrafficWatchRest.Model
 {
     public partial class Customer
     {
+        private int _id;
+        private string _googleId;
+        private string _email;
+        private string _firstName;
+        private string _lastName;
+        private bool _admin;
+        public Customer(int id, string googleId, string email, string firstName, string lastName, bool admin)
+        {
+            _id = id;
+            _googleId = googleId;
+            _email = email;
+            _firstName = firstName;
+            _lastName = lastName;
+            _admin = admin;
+        }
+
         public int Id { get; set; }
         public string GoogleId { get; set; }
         public string Email { get; set; }
@@ -13,7 +29,6 @@ namespace TrafficWatchRest.Model
         public int? AddressId { get; set; }
         public int? AlarmId { get; set; }
         public int? RouteId { get; set; }
-
         public bool? Administrator { get; set; }
     }
 }
