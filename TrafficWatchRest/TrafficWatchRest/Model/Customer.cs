5452de6c43c19ab77c87eb4e0fbfa0bb9f668d1c
Marcel Kristensen <36763180+Razhork@users.noreply.github.com>
2019-05-22 13:58:45 +0200

    Fjernet AdressCustomer & CUstomerAlarm variabler og ændret admin prop

--- TrafficWatchRest/TrafficWatchRest/Model/Customer.cs
+++ TrafficWatchRest/TrafficWatchRest/Model/Customer.cs
@@ -5,12 +5,6 @@ namespace TrafficWatchRest.Model
 {
     public partial class Customer
     {
-        public Customer()
-        {
-            AddressCustomer = new HashSet<AddressCustomer>();
-            CustomerAlarm = new HashSet<CustomerAlarm>();
-        }
-
         public int Id { get; set; }
         public string Email { get; set; }
         public string FirstName { get; set; }
@@ -18,9 +12,7 @@ namespace TrafficWatchRest.Model
         public int? AddressId { get; set; }
         public int? AlarmId { get; set; }
         public int? RouteId { get; set; }
-        public bool Administrator { get; set; }
 
-        public virtual ICollection<AddressCustomer> AddressCustomer { get; set; }
-        public virtual ICollection<CustomerAlarm> CustomerAlarm { get; set; }
+        public bool? Administrator { get; set; }
     }
 }
